343d5fef8bb334325a7ec5f47361e18b5ebff817
DmanYV <dima-xxx@mail.ru>
2023-01-30 14:15:50 +0300

    move code from course 1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # qa_guru_python_3_12
+
+# (Jenkins +Allure + Telegram)
+
---
+++ tests/simple/test_skipped.py
@@ -0,0 +1,9 @@
+import pytest
+
+@pytest.mark.skip
+def test_skipped_1():
+    pass
+
+@pytest.mark.skip
+def test_skipped_2():
+    pass
